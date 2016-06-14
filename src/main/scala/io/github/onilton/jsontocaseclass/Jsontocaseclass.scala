641cdce2c732b1ec129015545bed1e676ffb1001
Onilton Maciel <oniltonmaciel@gmail.com>
2016-06-14 19:56:33 -0300

    Remove old templates

--- src/main/scala/io/github/onilton/jsontocaseclass/Jsontocaseclass.scala
+++ src/main/scala/io/github/onilton/jsontocaseclass/Jsontocaseclass.scala
@@ -35,12 +35,6 @@ object Jsontocaseclass extends js.JSApp {
     val eleme = dom.document.getElementById("app")
     println(eleme.outerHTML)
 
-    $("#caseclassform textarea").change( (e: org.querki.jquery.JQueryEventObject) => {
-      val finalCode = $(e.target).valueString
-      $("#mycodeis").html(t.scalaCode(finalCode))
-      g.sh_highlightDocument()
-    })
-
     $("#test_button").click(() => {
       $("#pastejsonform textarea").value("""[{"coordinates":null,"favorited":false,"truncated":false,"created_at":"Mon Sep 03 13:24:14 +0000 2012","id_str":"242613977966850048","entities":{"urls":[],"hashtags":[],"user_mentions":[{"name":"Jason Costa","id_str":"14927800","id":14927800,"indices":[0,11],"screen_name":"jasoncosta"},{"name":"Matt Harris","id_str":"777925","id":777925,"indices":[12,26],"screen_name":"themattharris"},{"name":"ThinkWall","id_str":"117426578","id":117426578,"indices":[109,119],"screen_name":"thinkwall"}]},"in_reply_to_user_id_str":"14927800","contributors":null,"text":"@jasoncosta @themattharris Hey! Going to be in Frisco in October. Was hoping to have a meeting to talk about @thinkwall if you're around?","retweet_count":0,"in_reply_to_status_id_str":null,"id":242613977966850048,"geo":null,"retweeted":false,"in_reply_to_user_id":14927800,"place":null,"user":{"profile_sidebar_fill_color":"EEEEEE","profile_sidebar_border_color":"000000","profile_background_tile":false,"name":"Andrew Spode Miller","profile_image_url":"http://a0.twimg.com/profile_images/1227466231/spode-balloon-medium_normal.jpg","created_at":"Mon Sep 22 13:12:01 +0000 2008","location":"London via Gravesend","follow_request_sent":false,"profile_link_color":"F31B52","is_translator":false,"id_str":"16402947","entities":{"url":{"urls":[{"expanded_url":null,"url":"http://www.linkedin.com/in/spode","indices":[0,32]}]},"description":{"urls":[]}},"default_profile":false,"contributors_enabled":false,"favourites_count":16,"url":"http://www.linkedin.com/in/spode","profile_image_url_https":"https://si0.twimg.com/profile_images/1227466231/spode-balloon-medium_normal.jpg","utc_offset":0,"id":16402947,"profile_use_background_image":false,"listed_count":129,"profile_text_color":"262626","lang":"en","followers_count":2013,"protected":false,"notifications":null,"profile_background_image_url_https":"https://si0.twimg.com/profile_background_images/16420220/twitter-background-final.png","profile_background_color":"FFFFFF","verified":false,"geo_enabled":true,"time_zone":"London","description":"Co-Founder/Dev (PHP/jQuery) @justFDI. Run @thinkbikes and @thinkwall for events. Ex tech journo, helps run @uktjpr. Passion for Linux and customises everything.","default_profile_image":false,"profile_background_image_url":"http://a0.twimg.com/profile_background_images/16420220/twitter-background-final.png","statuses_count":11550,"friends_count":770,"following":null,"show_all_inline_media":true,"screen_name":"spode"},"in_reply_to_screen_name":"jasoncosta","source":"<a href=\"http://www.journotwit.com\" rel=\"nofollow\">JournoTwit</a>","in_reply_to_status_id":null},{"coordinates":{"coordinates":[121.0132101,14.5191613],"type":"Point"},"favorited":false,"truncated":false,"created_at":"Mon Sep 03 08:08:02 +0000 2012","id_str":"242534402280783873","entities":{"urls":[],"hashtags":[{"text":"twitter","indices":[49,57]}],"user_mentions":[{"name":"Jason Costa","id_str":"14927800","id":14927800,"indices":[14,25],"screen_name":"jasoncosta"}]},"in_reply_to_user_id_str":null,"contributors":null,"text":"Got the shirt @jasoncosta thanks man! Loving the #twitter bird on the shirt :-)","retweet_count":0,"in_reply_to_status_id_str":null,"id":242534402280783873,"geo":{"coordinates":[14.5191613,121.0132101],"type":"Point"},"retweeted":false,"in_reply_to_user_id":null,"place":null,"user":{"profile_sidebar_fill_color":"EFEFEF","profile_sidebar_border_color":"EEEEEE","profile_background_tile":true,"name":"Mikey","profile_image_url":"http://a0.twimg.com/profile_images/1305509670/chatMikeTwitter_normal.png","created_at":"Fri Jun 20 15:57:08 +0000 2008","location":"Singapore","follow_request_sent":false,"profile_link_color":"009999","is_translator":false,"id_str":"15181205","entities":{"url":{"urls":[{"expanded_url":null,"url":"http://about.me/michaelangelo","indices":[0,29]}]},"description":{"urls":[]}},"default_profile":false,"contributors_enabled":false,"favourites_count":11,"url":"http://about.me/michaelangelo","profile_image_url_https":"https://si0.twimg.com/profile_images/1305509670/chatMikeTwitter_normal.png","utc_offset":28800,"id":15181205,"profile_use_background_image":true,"listed_count":61,"profile_text_color":"333333","lang":"en","followers_count":577,"protected":false,"notifications":null,"profile_background_image_url_https":"https://si0.twimg.com/images/themes/theme14/bg.gif","profile_background_color":"131516","verified":false,"geo_enabled":true,"time_zone":"Hong Kong","description":"Android Applications Developer,  Studying Martial Arts, Plays MTG, Food and movie junkie","default_profile_image":false,"profile_background_image_url":"http://a0.twimg.com/images/themes/theme14/bg.gif","statuses_count":11327,"friends_count":138,"following":null,"show_all_inline_media":true,"screen_name":"mikedroid"},"in_reply_to_screen_name":null,"source":"<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>","in_reply_to_status_id":null}]""")
       $("#pastejsonform").submit()
@@ -197,26 +191,6 @@ object Jsontocaseclass extends js.JSApp {
     }
   }
 
-  def generateScala(el: JQuery): Unit = {
-    val content = el.find(".one_class").mapElems { value =>
-      val jvalue = $(value)
-      val props = jvalue.find(".li").mapElems { v =>
-        val jv = $(v)
-        val sst = jv.find("input.typescala").valueString
-        val finalSst = if (jv.find("""input.optional_value[type="checkbox"]""").prop("checked").orNull.asInstanceOf[Boolean]) {
-          s"Option[$sst]"
-        } else sst
-        "  " + sanitizeVarName(jv.find("label.keyname").text()) + ": " + finalSst
-      }
-      t.oneScalaCClass(jvalue.find("input.class_name").valueString, props.mkString(",\n"))
-    }.mkString("\n")
-
-    $("#caseclassform textarea").value(content)
-    $("#mycodeis").html(t.scalaCode(content))
-
-    g.sh_highlightDocument()
-  }
-
   def majName(e: dom.Element) = {
     val elem = $(e)
     val tochange = $("""div.ul input[data-signature-class="""" + elem.attr("data-signature-class") + """"]""")
@@ -266,42 +240,4 @@ object Jsontocaseclass extends js.JSApp {
     else
       newOname
   }
-
-  object t {
-    def alert(value: String) = s"""<div class="alert">
-      <button type="button" class="close" data-dismiss="alert">&times;</button>
-      <i class="icon-warning-sign"></i> $value
-      </div>"""
-    def error(value: String) =  s"""<div class="alert alert-error">
-      <button type="button" class="close" data-dismiss="alert">&times;</button>
-      <i class="icon-warning-sign"></i> $value
-      </div>"""
-    def info(value: String) = s"""<div class="alert alert-info">
-      <button type="button" class="close" data-dismiss="alert">&times;</button>
-      $value
-      </div>"""
-    def oneLine(name: String, typescala: String, sha: String, disabled: String, list: String, oname: String) =
-      s"""<div class="li control-group">
-      <label class="keyname control-label">$name</label>
-      <div class="controls">
-      <div class="input-append"><input class="typescala" $disabled type="text" data-signature-class="$sha" data-list="$list" value="$typescala" />
-       <span class="add-on">
-         <input class="optional_value" type="checkbox" value="" id="chkb_${oname}_$name" /><label class="label_chkbr" for="chkb_${oname}_$name"> optional</label>
-       </span>
-      </div>
-      </div>
-      </div>
-      """
-    def oneClass(oname: String, sha: String) = s"""<div id="class_$sha" class="one_class">
-      <fieldset>
-      <div class="class_title"><i class="icon-leaf"></i> <input class="class_name" data-signature-class="$sha" type="text" value="$oname" /></div>
-      <div class="ul"></div>
-      </fieldset>
-      </div>"""
-
-    def oneScalaCClass(cname: String, ccontent: String) = s"""case class $cname(\n$ccontent\n)"""
-    //one_scala_props : _.template('<%= pname %>\:<%= ptype %>'),
-    def scalaCode(code: String) = s"""<pre class="sh_scala">$code</pre>"""
-  }
-
 }
